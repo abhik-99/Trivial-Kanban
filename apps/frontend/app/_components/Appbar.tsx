dc1697c04ef0611a70ef6094cd1d5e04cbb61853
abhik-bits <127435906+abhik-bits@users.noreply.github.com>
2023-08-24 23:48:06 +0530

    Project Start Commit.

--- apps/frontend/app/_components/Appbar.tsx
+++ apps/frontend/app/_components/Appbar.tsx
@@ -1,15 +1,10 @@
-"use client"
+"use client";
 import React from "react";
 import SignoutButton from "./SignoutButton";
-import { useSession } from "next-auth/react";
 
 const Appbar = () => {
-
-  const {data: session, status} = useSession()
-  console.log("From App Bar" ,status, session)
-
   return (
-    <header className="flex gap-4 p-4 bg-gradient-to-b from-white to-gray-600 shadow">
+    <header className="flex justify-end gap-4 p-4 bg-gradient-to-b from-blue-900 to-black shadow">
       <SignoutButton />
     </header>
   );
